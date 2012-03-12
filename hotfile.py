1deb8e0482922e18312147009e84b7adf18e79f2
oczkers <oczkers@gmail.com>
2012-03-13 00:43:25 +0100

    little cleanup

--- hotfile.py
+++ hotfile.py
@@ -5,8 +5,8 @@ import requests
 import datetime
 import time
 import re
-from config import *
 import os
+from config import *
 
 def geturl(link, login, passwd):
 	opera = requests.session(headers=headers)
