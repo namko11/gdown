0a6615b3da9b4759bad327c856d6a208410dfb29
oczkers <oczkers@gmail.com>
2012-03-14 02:33:01 +0100

    uploadstation: upload is not working yet

--- uploadstation.py
+++ uploadstation.py
@@ -9,21 +9,19 @@ from config import *
 
 def geturl(link, login, passwd):
 	opera = requests.session(headers=headers)
-	values = {'loginUserName':login, 'loginUserPassword':passwd, 'loginFormSubmit':'Login'}
+	values = {'loginUserName':login, 'loginUserPassword':passwd, 'loginFormSubmit':'Login', 'autoLogin':'on'}
 	opera.post('http://www.uploadstation.com/login.php', values)
 	return opera.get(link).url	# return connection	
 
 def upload(login, passwd, filename):
 	opera = requests.session(headers=headers)
-	values = {'loginUserName':login, 'loginUserPassword':passwd, 'loginFormSubmit':'Login'}
+	values = {'loginUserName':login, 'loginUserPassword':passwd, 'loginFormSubmit':'Login', 'autoLogin':'on'}
 	opera.post('http://uploadstation.com/login.php', values)
 	content = opera.get('http://uploadstation.com/upload.php').content
-	host = re.search('action="(http://upload.uploadstation.com/upload/[0-9]+/[0-9]+/)"', content).group(1)
-	#
-	
-	# jquery getJSON function(data)
-	
-	#
-	content = opera.post(host, files={'file':open(filename, 'rb')}).content	# upload
+	print content
+	host = re.search('action="(http://upload.uploadstation.com/upload/[0-9]+/[0-9]+/)"', content).group(1)	
+	content = opera.get('%s?callback=jsonp' %(host)).content	# get sessionId
+	sessionId = re.search("sessionId:'(.+)'", content).group(1)
+	content = opera.post('%s%s' %(host, sessionId), files={'file':open(filename, 'rb')}).content
 	open('log.log', 'w').write(content)
 	return 0
