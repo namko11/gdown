642207f571417edb027e7857b215767ef8d8c589
oczkers <oczkers@gmail.com>
2012-03-15 10:45:04 +0100

    oron: add proxy support

--- oron.py
+++ oron.py
@@ -24,15 +24,16 @@ def geturl(link, login, passwd):
 	link = re.search('<a href="(.+)" class="atitle">Download File</a>', content).group(1)
 	return opera.get(link).url	# return connection
 
-def upload(login, passwd, filename):
+def upload(login, passwd, filename, proxy=None):
+	if proxy:	proxy = {'http':proxy, 'https':proxy}
 	opera = requests.session(headers=headers)
 	values = { 'login':login, 'password':passwd, 'op':'login', 'redirect':'', 'rand':'' }	# redirect is not working?
-	opera.post('http://oron.com/login', values)
-	content = opera.get('http://oron.com/').content
+	opera.post('http://oron.com/login', values, proxies=proxy)
+	content = opera.get('http://oron.com/', proxies=proxy).content
 	srv_id = re.search('name="srv_id" value="([0-9]+)"', content).group(1)
 	sess_id = re.search('name="sess_id" value="(.+)"', content).group(1)
 	srv_tmp_url = re.search('name="srv_tmp_url" value="(http://.+.oron.com)"', content).group(1)
-	content = opera.post('%s/upload/%s' %(srv_tmp_url, srv_id), {'upload_type':'file', 'srv_id':srv_id, 'sess_id':sess_id, 'srv_tmp_url':srv_tmp_url, 'link_rcpt':'', 'link_pass':'', 'tos':'1', 'submit_btn':' Upload! '}, files={'file_1':open(filename, 'rb')}).content	# upload
+	content = opera.post('%s/upload/%s' %(srv_tmp_url, srv_id), {'upload_type':'file', 'srv_id':srv_id, 'sess_id':sess_id, 'srv_tmp_url':srv_tmp_url, 'link_rcpt':'', 'link_pass':'', 'tos':'1', 'submit_btn':' Upload! '}, files={'file_1':open(filename, 'rb')}, proxies=proxy).content	# upload
 	file_id = re.search("name='fn' value='(.+?)'", content).group(1)
 	return 'http://oron.com/%s' %(file_id)
 	
