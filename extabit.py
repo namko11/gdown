9b3efccdf4273be5651bfff8721799d7270582be
oczkers <oczkers@gmail.com>
2012-03-19 19:59:17 +0100

    add extabit (not finished)

---
+++ extabit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import datetime
+import time
+import re
+import os
+from config import *
+
+def upload(login, passwd, filename):
+	#file_size = os.path.getsize(filename)	# get file size
+	opera = requests.session(headers=headers)
+	
+	
+	host = opera.get('http://api.hotfile.com/?action=getuploadserver').content[:-1]																				# get server to upload
+	upload_id = opera.post('http://%s/segmentupload.php?action=start' %(host), {'size':file_size}).content[:-1]													# start upload
+	opera.post('http://%s/segmentupload.php?action=upload' %(host), {'id':upload_id, 'offset':0}, files={'segment':open(filename, 'rb')}).content				# upload
+	return opera.post('http://%s/segmentupload.php?action=finish' %(host), {'id':upload_id, 'name':filename, 'username':login, 'password':passwd}).content[:-1]	# start upload
+	
