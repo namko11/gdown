f614453507b5ad0d880797879242d12f43c032d8
oczkers <oczkers@gmail.com>
2012-04-30 21:35:01 +0200

    netload: better status catching

--- netload.py
+++ netload.py
@@ -20,16 +20,17 @@ def status(login, passwd):
 	if 'This account was locked' in content or 'not found in our records!' in content or 'Invald Password' in content or 'Invalid User ID!' in content:
 		return -1
 	elif 'Sorry, please activate first your account.' in content:	# account not activated
-		return 0
+		return time.time()
 	elif 'Please wait a moment before tryingto log in again!' in content:	# ip blocked
 		print 'ip blocked'
 		asd
 		return -2
 	content = opera.get('http://netload.in/index.php?id=2').content
 	if 'No Bonus' in content or 'Kein Premium' in content:
-		return 0
+		return time.time()
 	else:
 		return time.time() + int(re.search('<div style="float: left; width: 150px; color: #FFFFFF;"><span style="color: green">([0-9]+) Tage?.+</span></div>', content).group(1))*60*60*24
+	print content
 
 def upload(login, passwd, filename):
 	opera = requests.session(headers=headers)
