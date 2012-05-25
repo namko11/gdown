c5e56021f3d290a24666505ab64f2a6265cd06fd
oczkers <oczkers@gmail.com>
2012-05-25 02:11:08 +0200

    hotfile: little cleanup

--- pypremium/hotfile.py
+++ pypremium/hotfile.py
@@ -16,16 +16,17 @@ def geturl(link, login, passwd):
 def status(login, passwd):
 	opera = requests.session(headers=headers)
 	content = opera.get('http://api.hotfile.com/?action=getuserinfo&username=%s&password=%s' %(login, passwd)).content
-	if 'is_premium=1' in content:
+	if 'is_premium=1' in content:	# premium
 		return time.mktime(datetime.datetime.strptime(re.search('premium_until=(.+?)&', content).group(1)[:-6],'%Y-%m-%dT%H:%M:%S').timetuple())
-	elif 'is_premium=0' in content:
-		return time.time()
-	elif 'invalid username or password' in content or 'user account is suspended' in content:
+	elif 'is_premium=0' in content:	# free
+		return 0
+	elif 'invalid username or password' in content or 'user account is suspended' in content:	# invalid login/passwd or account suspended
 		return -1
-	elif 'too many failed attemtps' in content:
+	elif 'too many failed attemtps' in content:	# ip blocked
 		print 'ip blocked'
 		asd
-	print content
+	else:
+		open('log.log', 'w').write(content)
 
 def upload(login, passwd, filename):
 	file_size = os.path.getsize(filename)	# get file size
