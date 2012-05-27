d77de341bcf24420f458c78cd2fb7d7e914aac7c
oczkers <oczkers@gmail.com>
2012-05-27 13:44:04 +0200

    fix imports in __init__

--- pypremium/__init__.py
+++ pypremium/__init__.py
@@ -30,35 +30,3 @@ __all__ = [
 	'youporn',
 	'youtube',
 	]
-
-#import 4shared
-import bitshare
-#import chomikuj
-import crocko
-import depositfiles
-#import easyshare
-#import enterupload
-import filefactory
-import freakshare
-#import gigasize
-import hotfile
-import letitbit
-import mediafire
-import megashares
-import netload
-import oron
-import pornhub
-import rapidshare
-import redtube
-import shareonline
-#import soundcloud
-import turbobit
-import uploaded
-#import uploading
-import uploadstation
-#import usershare
-import xhamster
-import xvideos
-import youjizz
-import youporn
-import youtube
