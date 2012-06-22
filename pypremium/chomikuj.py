1675d60add5855dec914e50ea5c333e648f33100
oczkers <oczkers@gmail.com>
2012-06-22 11:39:21 +0200

    .

--- pypremium/chomikuj.py
+++ pypremium/chomikuj.py
@@ -13,8 +13,8 @@ def geturl(link, login, passwd):
 	token = re.search('name="__RequestVerificationToken" type="hidden" value="(.*?)"', content).group(1)
 	# login
 	content = opera.post('http://chomikuj.pl/action/Login/TopBarLogin', {'__RequestVerificationToken':token, 'ReturnUrl':link, 'Login':login, 'Password':passwd, 'rememberLogin':'true', 'topBar_LoginBtn':'Zaloguj'}).content
+	# get download url
 	fileId = re.search('name="FileId" value="([0-9]+)"', content).group(1)
 	token = re.search('name="__RequestVerificationToken" type="hidden" value="(.*?)"', content).group(1)
-	# get download url
 	content = opera.post('http://chomikuj.pl/action/License/Download', {'fileId':fileId, '__RequestVerificationToken':token}).content
 	return re.search('"redirectUrl":"(.*?)"', content).group(1)
