268b60ea5705d615ac5becdf9def098358c5f43c
oczkers <oczkers@gmail.com>
2013-01-23 01:08:32 +0100

    tests

--- test_pypremium.py
+++ test_pypremium.py
@@ -1,12 +1,15 @@
 ﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+'''tests for pypremium'''
+
 import unittest
 
 import pypremium
 
-class RequestsTestCase(unittest.TestCase):
+class PypremiumTestCase(unittest.TestCase):
 	
-	#_multiprocess_can_split_ = True
+	_multiprocess_can_split_ = True
 	
 	def setUp(self):
 		"""Create simple data set with headers."""
@@ -22,9 +25,17 @@ class RequestsTestCase(unittest.TestCase):
 	def test_entry_points(self):
 		pypremium.modules
 	
-	def test_bitshare(self):
-		'''free account'''
-		assert pypremium.bitshare.status('oczkers', 'mandrake') == 0
+	def testStatus(self):
+		'''Tests status function on free account'''
+		for module in modules():
+			if hasattr(module, 'status'):
+				self.assertEqual(module.status('oczkers', 'mandrake'), 0)
+
+def modules():
+	modules_list = []
+	for module_name in pypremium.modules.__all__:
+		modules_list.append(getattr(pypremium, module_name))
+	return modules_list
 
 if __name__ == '__main__':
 	unittest.main()
