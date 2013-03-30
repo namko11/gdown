3433b2af2e5919d18fb4e014b4632131f4536953
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-03-30 10:28:21 +0100

    bump user-agent

--- gdown/config.py
+++ gdown/config.py
@@ -1,9 +1,9 @@
 headers = {
-    'User-Agent': 'Opera/9.80 (X11; Linux x86_64; U; en) Presto/2.12.388 Version/12.11',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 Safari/537.31',
     'Accept': 'text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1',
     'Accept-Encoding': 'deflate, gzip, x-gzip, identity, *;q=0',
     'Accept-Language': 'en;q=0.8',
-    'Accept-Charset': 'iso-8859-1, utf-8, utf-16, *;q=0.1',
+    'Accept-Charset': 'utf-8, iso-8859-1, utf-16, *;q=0.1',
     'Connection': 'Keep-Alive, TE',
     'Keep-Alive': '300',
     'TE': 'deflate, gzip, chunked, identity, trailers',
