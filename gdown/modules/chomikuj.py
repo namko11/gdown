da31d90da248c8a3260b86c33087af62d0fdcfee
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-04-10 18:43:25 +0200

    chomikuj: add accInfo (detect points & transfer only)

--- gdown/modules/chomikuj.py
+++ gdown/modules/chomikuj.py
@@ -10,7 +10,25 @@ This module contains handlers for chomikuj.
 
 import re
 
-from ..module import browser
+from ..module import browser, acc_info_template
+
+
+def accInfo(username, passwd):
+    """Returns account info."""
+    # TODO: detect premium acc
+    acc_info = acc_info_template()
+    r = browser()
+    rc = r.get('http://chomikuj.pl').content
+    token = re.search('name="__RequestVerificationToken" type="hidden" value="(.*?)"', rc).group(1)
+    data = {'__RequestVerificationToken': token, 'ReturnUrl': '', 'Login': username, 'Password': passwd, 'rememberLogin': 'true', 'topBar_LoginBtn': 'Zaloguj'}
+    rc = r.post('http://chomikuj.pl/action/Login/TopBarLogin', data).content
+
+    transfer = float(re.search('title="Transfer" rel="nofollow"><strong>([0-9,]+) MB</strong>', rc).group(1).replace(',', '.'))
+    acc_info['transfer'] = int(transfer * 1024 * 1024)  # convert MB to B
+
+    acc_info['points'] = int(re.search('title="Punkty" rel="nofollow"><strong>([0-9]+)</strong>', rc).group(1))
+
+    return acc_info
 
 
 def getUrl(link, username, passwd):
