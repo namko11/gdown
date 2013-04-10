1d094f0a5b1cd71d74078c166d10e298402d9db9
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-04-10 18:30:59 +0200

    chomikuj: cleanup

--- gdown/modules/chomikuj.py
+++ gdown/modules/chomikuj.py
@@ -15,14 +15,14 @@ from ..module import browser
 
 def getUrl(link, username, passwd):
     """Returns direct file url."""
-    opera = browser()
-    # get token
-    content = opera.get('http://chomikuj.pl').content
-    token = re.search('name="__RequestVerificationToken" type="hidden" value="(.*?)"', content).group(1)
-    # login
-    content = opera.post('http://chomikuj.pl/action/Login/TopBarLogin', {'__RequestVerificationToken': token, 'ReturnUrl': link, 'Login': username, 'Password': passwd, 'rememberLogin': 'true', 'topBar_LoginBtn': 'Zaloguj'}).content
+    r = browser()
+    rc = r.get('http://chomikuj.pl').content
+    token = re.search('name="__RequestVerificationToken" type="hidden" value="(.*?)"', rc).group(1)
+    data = {'__RequestVerificationToken': token, 'ReturnUrl': link, 'Login': username, 'Password': passwd, 'rememberLogin': 'true', 'topBar_LoginBtn': 'Zaloguj'}
+    rc = r.post('http://chomikuj.pl/action/Login/TopBarLogin', data).content
     # get download url
-    fileId = re.search('name="FileId" value="([0-9]+)"', content).group(1)
-    token = re.search('name="__RequestVerificationToken" type="hidden" value="(.*?)"', content).group(1)
-    content = opera.post('http://chomikuj.pl/action/License/Download', {'fileId': fileId, '__RequestVerificationToken': token}).content
-    return re.search('"redirectUrl":"(.*?)"', content).group(1)
+    fileId = re.search('name="FileId" value="([0-9]+)"', rc).group(1)
+    token = re.search('name="__RequestVerificationToken" type="hidden" value="(.*?)"', rc).group(1)
+    data = {'fileId': fileId, '__RequestVerificationToken': token}
+    rc = r.post('http://chomikuj.pl/action/License/Download', data).content
+    return re.search('"redirectUrl":"(.*?)"', rc).group(1)
