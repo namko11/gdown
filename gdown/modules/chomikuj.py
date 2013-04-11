798083344f3314aa24fa1e30aab0ddb8c0345261
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-04-11 13:53:16 +0200

    chomikuj: detect premium/free account type

--- gdown/modules/chomikuj.py
+++ gdown/modules/chomikuj.py
@@ -9,19 +9,26 @@ This module contains handlers for chomikuj.
 """
 
 import re
+from dateutil import parser
 
 from ..module import browser, acc_info_template
 
 
 def accInfo(username, passwd):
     """Returns account info."""
-    # TODO: detect premium acc
     acc_info = acc_info_template()
     r = browser()
     rc = r.get('http://chomikuj.pl').content
     token = re.search('name="__RequestVerificationToken" type="hidden" value="(.*?)"', rc).group(1)
     data = {'__RequestVerificationToken': token, 'ReturnUrl': '', 'Login': username, 'Password': passwd, 'rememberLogin': 'true', 'topBar_LoginBtn': 'Zaloguj'}
     rc = r.post('http://chomikuj.pl/action/Login/TopBarLogin', data).content
+
+    status = re.search('Abonament Twojego Chomika jest ważny do: <h3>([0-9]{4}\-[0-9]{2}\-[0-9]{2})</h3>', rc)
+    if status:
+        acc_info['status'] = 'premium'
+        acc_info['expire_date'] = parser.parse(status.group(1))
+    else:
+        acc_info['status'] = 'free'
 
     transfer = float(re.search('title="Transfer" rel="nofollow"><strong>([0-9,]+) MB</strong>', rc).group(1).replace(',', '.'))
     acc_info['transfer'] = int(transfer * 1024 * 1024)  # convert MB to B
