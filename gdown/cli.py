e4da26233b3b891263739c368aeadcc2fe8e58d7
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-05-21 12:27:53 +0200

    init kat module (accInfo & voteGood)

--- gdown/cli.py
+++ gdown/cli.py
@@ -4,8 +4,7 @@
 """gdown (command-line interface to the gdown library)
 
 Usage:
-    gdown
-    gdown -h | --help | --version
+    gdown <command> <module> [(-u <username> -p <password>)]
 
 Options:
     -h --help  Show this screen.
@@ -14,10 +13,29 @@ Options:
 """
 
 from docopt import docopt
+from clint.textui import colored
 
-from . import __title__, __version__, __author__, __license__, __copyright__
+#from . import __title__, __version__, __author__, __license__, __copyright__
+#from . import modules
+import modules
+__version__ = '0.0.1'
+
+
+def accInfo(username, passwd):
+    """Return account info."""
+    pass
 
 
 def main():
-    arguments = docopt(__doc__, version=__version__)
-    print arguments
+    args = docopt(__doc__, version=__version__)
+    print args  # DEBUG
+    if args['<command>'].lower() == 'accinfo':
+        if hasattr():  # module has attribute?
+            #accInfo(username, passwd)
+            pass
+        else:
+            print 'Sorry, %s module does not support this command yet.' % args['<module>']
+
+
+if __name__ == '__main__':
+    main()
--- gdown/modules/kat.py
+++ gdown/modules/kat.py
@@ -8,5 +8,43 @@ This module contains handlers for kat.
 
 """
 
+import re
+from simplejson import JSONDecoder
 
-# nobody loves me ;-(
+from ..module import browser, acc_info_template
+#from ..exceptions import ModuleError, IpBlocked
+
+
+def __login__(username, passwd):
+    """Returns requests object after logging in."""
+    r = browser()
+    data = {'return_uri': 'https://kat.ph/', 'email': username, 'password': passwd}
+    rc = r.post('https://kat.ph/auth/socialize/', data).content
+    open('log.log', 'w').write(rc)
+    # TODO: validate login
+    return r
+
+
+def voteGood(username, passwd, torrent_hash):
+    """Vote good."""
+    r = __login__(username, passwd)
+    rc = JSONDecoder().decode(r.post('https://kat.ph/torrents/submitthnx/{}/'.format(torrent_hash)).content)
+    if rc['method'] == 'ok':
+        return True
+    else:
+        open('log.log', 'w').write(rc)
+        return False
+
+
+def accInfo(username, passwd):
+    """Returns account info."""
+    voteGood(username, passwd, '872C9D5EF32EEE1F6BCCC69E53E69E7254CC9A32')
+    acc_info = acc_info_template()
+    r = __login__(username, passwd)
+    rc = r.get('https://kat.ph/').content
+    username = re.search('<a href="/user/(.+)/">profile</a>', rc).group(1)
+    rc = r.get('https://kat.ph/user/{}'.format(username)).content
+    rating = re.search('<span title="Reputation" class="repValue positive">([0-9]+)</span>', rc).group(1)
+    print username, rating
+    acc_info['status'] = 'free'
+    return acc_info
