c9636f1f1fa707982b5fe25dc13aa4f8b35d7891
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-09-14 12:33:16 +0200

    filefactory: update for new design

--- gdown/modules/filefactory.py
+++ gdown/modules/filefactory.py
@@ -19,7 +19,7 @@ from ..exceptions import ModuleError
 def upload(username, passwd, filename):
     """Returns uploaded file url."""
     r = browser()
-    r.post('http://www.filefactory.com/member/login.php', {'email': username, 'password': passwd})  # login to get ff_membership cookie
+    r.post('http://www.filefactory.com/member/signin.php', {'loginEmail': username, 'loginPassword': passwd, 'Submit': 'Sign In'})  # login to get ff_membership cookie
     #host = r.get('http://www.filefactory.com/servers.php?single=1').content  # get best server to upload
     host = 'http://upload.filefactory.com/upload.php'  # always returning the same url (?)
     viewhash = re.search('<viewhash>(.+)</viewhash>', r.get('http://www.filefactory.com/upload/upload_flash_begin.php?files=1').content).group(1)  # get viewhash
@@ -31,19 +31,25 @@ def accInfo(username, passwd, proxy=False):
     """Returns account info."""
     acc_info = acc_info_template()
     r = browser(proxy)
-    content = r.post('http://www.filefactory.com/member/login.php', {'redirect': '/', 'email': username, 'password': passwd, 'socialID': '', 'socialType': 'facebook'}).content
-    if '<p class="greenText">Free member</p>' in content:
+    content = r.post('http://www.filefactory.com/member/signin.php', {'loginEmail': username, 'loginPassword': passwd, 'Submit': 'Sign In'}).content
+
+    if 'What is your date of birth?' in content:
+        raise ModuleError('Birth date not set.')
+    #    print 'db'  # DEBUG
+    #    content = r.post('http://www.filefactory.com/member/setdob.php', {'newDobMonth': '1', 'newDobDay': '1', 'newDobYear': '1970', 'Submit': 'Continue'}).content
+
+    if '<strong>Free Member</strong>' in content:
         acc_info['status'] = 'free'
         return acc_info
-    elif any(i in content for i in ('The account you are trying to use has been deleted.', 'This account has been automatically suspended due to account sharing.')):
+    elif any(i in content for i in ('The account you are trying to use has been deleted.', 'This account has been automatically suspended due to account sharing.', 'The account you have tried to sign into is pending deletion.')):
         acc_info['status'] = 'blocked'
         return acc_info
-    elif 'The email or password you have entered is incorrect' in content:
+    elif any(i in content for i in ('The email or password you have entered is incorrect', 'The email or password wre invalid.  Please try again.')):
         acc_info['status'] = 'deleted'
         return acc_info
-    elif '<span class="greenText">Premium until <time datetime=' in content:
+    elif 'title="Premium valid until:' in content:
         acc_info['status'] = 'premium'
-        acc_info['expire_date'] = parser.parse(re.search('<span class="greenText">Premium until <time datetime="([0-9]{4}\-[0-9]{2}\-[0-9]{2})">', content).group(1))
+        acc_info['expire_date'] = parser.parse(re.search('title="Premium valid until: <strong>(.+?)</strong>">', content).group(1))
         return acc_info
     elif "Congratulations! You're a FileFactory Lifetime member. We value your loyalty and support." in content:
         acc_info['status'] = 'premium'
