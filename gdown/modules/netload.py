390f8dad083cf57a92c2a412714199f4225cdc06
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2015-04-09 00:46:45 +0200

    netload: fix

--- gdown/modules/netload.py
+++ gdown/modules/netload.py
@@ -19,8 +19,8 @@ def getUrl(link, username, passwd):
     """Returns direct file url.
     IP validator is present."""
     r = browser()
-    values = {'txtuser': username, 'txtpass': passwd, 'txtcheck': 'login', 'txtlogin': ''}
-    r.post('http://netload.in/index.php', values)
+    values = {'txtuser': username, 'txtpass': passwd, 'txtcheck': 'login', 'txtlogin': 'Login'}
+    r.post('https://netload.in/index.php', values)
     return r.get(link).url  # return connection
 
 
@@ -29,8 +29,8 @@ def accInfo(username, passwd, proxy=False):
     acc_info = acc_info_template()
     r = browser(proxy)
     values = {'txtuser': username, 'txtpass': passwd, 'txtcheck': 'login', 'txtlogin': 'Login'}
-    r.post('http://netload.in/index.php', values)
-    content = r.get('http://netload.in/index.php?id=15').content
+    r.post('https://netload.in/index.php', values)
+    content = r.get('https://netload.in/index.php?id=15').content
     if 'This account was locked' in content or'Sorry, please activate first your account.' in content:  # account not activated
         acc_info['status'] = 'blocked'
         return acc_info
@@ -39,19 +39,20 @@ def accInfo(username, passwd, proxy=False):
         return acc_info
     elif 'Please wait a moment before tryingto log in again!' in content:   # ip blocked
         raise IpBlocked
-    content = r.get('http://netload.in/index.php?id=2').content
-    if 'No Bonus' in content or 'Kein Premium' in content:
+    rc = r.get('https://netload.in/index.php?id=2').content
+    content = re.search('<div class="num">([0-9d, h\-]+?)</div>\n[ ]{16}<h3>Remaining Premium</h3>', rc).group(1)
+    if content == '-':
         acc_info['status'] = 'free'
-        return acc_info
     else:
-        # blind guess?
-        content = re.search('<div style="float: left; width: 150px; color: #FFFFFF;"><span style="color: green">([0-9]*?)[ Tage,]{,7}([0-9]+) Stunden</span></div>', content)
         acc_info['status'] = 'premium'
-        if content.group(1):
-            acc_info['expire_date'] = datetime.utcnow() + timedelta(days=int(content.group(1)), hours=int(content.group(2)))
-        else:
-            acc_info['expire_date'] = datetime.utcnow() + timedelta(hours=int(content.group(2)))
-        return acc_info
+        acc_info['expire_date'] = datetime.utcnow()
+        content = content.split(', ')
+        for i in content:
+            if i.endswith('d'):  # days
+                acc_info['expire_date'] = acc_info['expire_date'] + timedelta(days=int(i[:-1]))
+            elif i.endswith('h'):  # hours
+                acc_info['expire_date'] = acc_info['expire_date'] + timedelta(hours=int(i[:-1]))
+    return acc_info
 
 
 def upload(username, passwd, filename):
