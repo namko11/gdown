43f0863fb7b64526db90bb00dd66f459a1d5d6c6
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-05-20 16:37:03 +0000

    catshare: detect <1 day premium

--- gdown/modules/catshare.py
+++ gdown/modules/catshare.py
@@ -16,7 +16,6 @@ from decimal import Decimal
 from ..module import browser, acc_info_template
 
 
-
 def accInfo(username, passwd, proxy=False):
     """Returns account info."""
     acc_info = acc_info_template()
@@ -28,25 +27,28 @@ def accInfo(username, passwd, proxy=False):
     elif '<span style="color: red">Darmowe</span>' in rc:
         acc_info['status'] = 'free'
     elif '<span class="hidden-xs">Premium</span>' in rc:
+        expire_date = datetime.utcnow()
         acc_info['status'] = 'premium'
         open('gdown.log', 'w').write(rc)  # DEBUG
-        s = re.search('<span class="hidden-xs">Premium</span> <b>([0-9]+) dni</b> \( ([0-9\.]+ GB) \)', rc)
-        expire_date = datetime.utcnow()
-        '''
-        if seconds:
-            expire_date += timedelta(seconds=int(seconds.group(1)))
-        if minutes:
-            expire_date += timedelta(minutes=int(minutes.group(1)))
-        if hours:
-            expire_date += timedelta(hours=int(hours.group(1)))
-        if days:
-            expire_date += timedelta(days=int(days.group(1)))
-        if weeks:
-            expire_date += timedelta(weeks=int(weeks.group(1)))
-        '''
-        expire_date += timedelta(days=int(s.group(1)))
-        acc_info['expire_date'] = expire_date
-        acc_info['transfer'] = s.group(2)
+        if 'godzin</span></b>' in rc:  # < 1 day
+            acc_info['expire_date'] = expire_date + timedelta(days=1)
+        else:
+            s = re.search('<span class="hidden-xs">Premium</span> <b>([0-9]+) dni</b> \( ([0-9\.]+ GB) \)', rc)
+            '''
+            if seconds:
+                expire_date += timedelta(seconds=int(seconds.group(1)))
+            if minutes:
+                expire_date += timedelta(minutes=int(minutes.group(1)))
+            if hours:
+                expire_date += timedelta(hours=int(hours.group(1)))
+            if days:
+                expire_date += timedelta(days=int(days.group(1)))
+            if weeks:
+                expire_date += timedelta(weeks=int(weeks.group(1)))
+            '''
+            expire_date += timedelta(days=int(s.group(1)))
+            acc_info['expire_date'] = expire_date
+            acc_info['transfer'] = s.group(2)
     else:
         open('gdown.log', 'w').write(rc)  # DEBUG
         asdasdadasd
