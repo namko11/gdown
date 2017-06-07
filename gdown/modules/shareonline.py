6b6fb3cd4fe03cf2136b2461648f4665bc7999e6
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-06-07 21:06:12 +0000

    shareonline: add accInfo

--- gdown/modules/shareonline.py
+++ gdown/modules/shareonline.py
@@ -10,8 +10,9 @@ This module contains handlers for shareonline.
 
 import re
 import os
+from dateutil import parser
 
-from ..module import browser
+from ..module import browser, acc_info_template
 
 
 def upload(username, passwd, filename):
@@ -24,3 +25,26 @@ def upload(username, passwd, filename):
     data = {'username': username, 'password': passwd, 'upload_session': upload_session, 'chunk_no': 1, 'chunk_number': 1, 'filesize': file_size, 'finalize': 1}
     content = r.post('http://%s' % (host), data, files={'fn': open(filename, 'rb')}).text  # upload
     return re.match('(.+);[0-9]+;.+', content).group(1)
+
+
+def accInfo(username, passwd, proxy=False):
+    r = browser(proxy)
+    acc_info = acc_info_template()
+    r.get('https://www.share-online.biz/lang/set/english')
+    data = {'user': username,
+            'pass': passwd}
+    rc = r.post('https://www.share-online.biz/user/login', data=data).text
+    open('gdown.log', 'w').write(rc)
+    if 'This account is disabled, please contact support' in rc:
+        acc_info['status'] = 'blocked'
+        return acc_info
+    elif 'Sammler        </p>' in rc:
+        acc_info['status'] = 'free'
+        return acc_info
+    elif 'Premium        </p>' in rc:
+        acc_info['status'] = 'premium'
+        expire_date = re.search("<span class='green'>([0-9\., :]+?)</span>", rc).group(1)  # this probably won't be "green" allways
+        acc_info['expire_date'] = parser.parse(expire_date)
+        return acc_info
+    else:
+        asddsadsa
