67d44c8b485a64e7933093d338669b09c54bfb32
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-07-03 16:00:05 +0000

    fichier: better detection

--- gdown/modules/fichier.py
+++ gdown/modules/fichier.py
@@ -20,13 +20,13 @@ def accInfo(username, passwd, proxy=False):
     data = {'mail': username, 'pass': passwd, 'valider': 'Send'}
     rc = r.post('https://1fichier.com/login.pl', data).text
     open('gdown.log', 'w').write(rc)
-    if 'Invalid username.' in rc or 'Invalid username or Password.' in rc:
+    if 'Invalid username.' in rc or 'Invalid email address.' in rc or 'Invalid username or Password.' in rc or 'Invalid password.' in rc:
         if 'Warning ! You have only 0 try left' in rc:
             print('NO MORE TRIES')
             asddsasaddsa
         acc_info['status'] = 'deleted'
         return acc_info
-    elif 'For security reasons, following many identification errors, your IP address (176.9.140.205) is temporarily locked.' in rc:
+    elif 'For security reasons, following many identification errors, your IP address (' in rc:
         print('ip AND ACC blocked')
         asdasdsad
     elif 'Logout' not in rc:
@@ -34,10 +34,18 @@ def accInfo(username, passwd, proxy=False):
         asdasddas
     rc = r.get('https://1fichier.com/console/abo.pl').text
     open('gdown.log', 'w').write(rc)
-    if 'Your Premium offer subscription is valid' not in rc:
+    if 'Your Premium offer subscription is valid until' in rc:  # premium
+        expire_date = re.search('Your Premium offer subscription is valid until <span style="font-weight:bold">([0-9]{4}\-[0-9]{2}\-[0-9]{2})</span>', rc).group(1)
+        acc_info['status'] = 'premium'
+        acc_info['expire_date'] = parser.parse(expire_date)
+        return acc_info
+    elif 'Your Access offer subscription is valid until' in rc:  # access (~half-premium)
+        expire_date = re.search('Your Access offer subscription is valid until <span style="font-weight:bold">([0-9]{4}\-[0-9]{2}\-[0-9]{2})</span>', rc).group(1)
+        acc_info['status'] = 'premium'
+        acc_info['expire_date'] = parser.parse(expire_date)
+        return acc_info
+    elif 'Your Premium offer subscription is valid' not in rc:
         acc_info['status'] = 'free'
         return acc_info
-    expire_date = re.search('Your Premium offer subscription is valid until <span style="font-weight:bold">([0-9]{4}\-[0-9]{2}\-[0-9]{2})</span>', rc).group(1)
-    acc_info['status'] = 'premium'
-    acc_info['expire_date'] = parser.parse(expire_date)
-    return acc_info
+    else:
+        asdasdasdqw
