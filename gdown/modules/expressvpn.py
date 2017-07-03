d2adda34bc313298ee1ad3c8171b8402d6c215ae
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-07-03 15:58:58 +0000

    expressvpn: detect premium & expired

--- gdown/modules/expressvpn.py
+++ gdown/modules/expressvpn.py
@@ -9,7 +9,9 @@ This module contains handlers for expressvpn.
 """
 
 import re
+from dateutil import parser
 from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
 
 from ..module import browser, acc_info_template
 from ..exceptions import ModuleError
@@ -30,3 +32,16 @@ def accInfo(username, passwd, proxy=False):
     r.headers['Referer'] = 'https://www.expressvpn.com/pl/users/sign_in'
     rc = r.post('https://www.expressvpn.com/users/sign_in', data=data).text
     open('gdown.log', 'w').write(rc)
+    rc = r.get('https://www.expressvpn.com/subscriptions').text
+    open('gdown.log', 'w').write(rc)
+    if 'Expires on' in rc:
+        bs = BeautifulSoup(rc, 'lxml')
+        expire_date = bs.find('div', class_='table-col c15').contents[2]
+        acc_info['status'] = 'premium'
+        acc_info['expire_date'] = parser.parse(expire_date)
+    elif 'This subscription expired' in rc:
+        acc_info['status'] = 'free'
+    else:
+        print('unknown status')
+        asdasdasdasd
+    return acc_info
