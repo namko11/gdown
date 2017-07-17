50c4f22725d2424399e64e0bbcf798f4edc9b355
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-07-17 13:30:52 +0000

    filesmonster: detect banned

--- gdown/modules/filesmonster.py
+++ gdown/modules/filesmonster.py
@@ -21,13 +21,14 @@ def accInfo(username, passwd, proxy=False):
     r = browser(proxy)
     data = {'act': 'login', 'user': username, 'pass': passwd, 'captcha_shown': 0, 'login': 'Login'}
     rc = r.post('https://filesmonster.com/login.php', data).text
-    if 'For security reasons, please enter captcha code below' in rc:
-        print('captcha')
+    open('gdown.log', 'w').write(rc)  # DEBUG
+    if 'For security reasons, please enter captcha code below' in rc or 'Please confirm that you are not a robot' in rc:
+        print('recaptcha')
         adassa
     elif 'Username/Password can not be found in our database!' in rc:
         acc_info['status'] = 'deleted'
         return acc_info
-    elif 'Your account is suspended!' in rc:
+    elif 'Your account is suspended!' in rc or 'Your account is banned!' in rc:
         acc_info['status'] = 'blocked'
         return acc_info
     elif "<span class='red em'>Expired:" in rc:
@@ -36,4 +37,3 @@ def accInfo(username, passwd, proxy=False):
         acc_info['status'] = 'free'
         return acc_info
     # Your membership type: <span class="em lightblack">Premium</span>
-    open('log.log', 'w').write(rc)  # DEBUG
