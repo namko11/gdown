57812f7b2e715ad9d7722d3acee44927a5aff00f
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-08-03 22:12:32 +0000

    ipvanish: detect ip banned

--- gdown/modules/ipvanish.py
+++ gdown/modules/ipvanish.py
@@ -27,7 +27,9 @@ def accInfo(username, passwd, proxy=False):
     rc = r.post('https://account.ipvanish.com/index.php', data=data).text
     open('gdown.log', 'w').write(rc)
     #     status = re.search('<b>Account Status:</b></span>\n    <span class="profile_label">(.+?)</span>', rc).group(1)
-    if '<span class="profile_label">Active</span>' in rc:
+    if 'Access Denied: Failed attempt limit reached.' in rc:
+        raise ModuleError('ip banned')
+    elif '<span class="profile_label">Active</span>' in rc:
         acc_info['status'] = 'premium'
         expire_date = re.search('Renewal Date:</b></span>\n<span class="profile_label">([0-9/]+?)</span>', rc).group(1)
         acc_info['expire_date'] = parser.parse(expire_date)
