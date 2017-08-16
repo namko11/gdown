a5f12ede78825cafc7678015dc4146888253e26a
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-08-16 18:13:17 +0000

    filesmonster fix free detection

--- gdown/modules/filesmonster.py
+++ gdown/modules/filesmonster.py
@@ -8,11 +8,11 @@ This module contains handlers for filesmonster.
 
 """
 
-import re
-from datetime import datetime
+# import re
+# from datetime import datetime
 
 from ..module import browser, acc_info_template
-from ..exceptions import ModuleError, IpBlocked
+from ..exceptions import ModuleError  # , IpBlocked
 
 
 def accInfo(username, passwd, proxy=False):
@@ -24,16 +24,17 @@ def accInfo(username, passwd, proxy=False):
     open('gdown.log', 'w').write(rc)  # DEBUG
     if 'For security reasons, please enter captcha code below' in rc or 'Please confirm that you are not a robot' in rc:
         print('recaptcha')
-        adassa
+        raise ModuleError('captcha')
     elif 'Username/Password can not be found in our database!' in rc:
         acc_info['status'] = 'deleted'
-        return acc_info
     elif 'Your account is suspended!' in rc or 'Your account is banned!' in rc:
         acc_info['status'] = 'blocked'
-        return acc_info
-    elif "<span class='red em'>Expired:" in rc:
+    # elif "<span class='red em'>Expired:" in rc:
+    elif 'Your membership type: <strong>Regular' in rc:
         # TODO: parse date
         # <span class='red em'>Expired: 06/25/12</span>
         acc_info['status'] = 'free'
-        return acc_info
+    else:
+        raise ModuleError('unknown status')
+    return acc_info
     # Your membership type: <span class="em lightblack">Premium</span>
