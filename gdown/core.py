276f7cc14afcbd68fa55dbcff59efbb76cc07da2
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-08-16 18:13:34 +0000

    recaptcha resolving for realdebrid using 2captcha.net

--- gdown/core.py
+++ gdown/core.py
@@ -7,12 +7,13 @@ gdown.core
 This module implements the gdown's basic methods.
 
 """
-import re
+# import re
 import sys
-from random import random
+import time
+# from random import random
 from io import BytesIO
 
-from .config import deathbycaptcha_username as decaptcha_username, deathbycaptcha_password as decaptcha_password
+from .config import deathbycaptcha_username as decaptcha_username, deathbycaptcha_password as decaptcha_password, twocaptcha_api_key
 # from .config import decaptchercom_username, decaptchercom_password
 from .module import browser
 if sys.version_info[0] == 2 or '__pypy__' in sys.builtin_module_names:
@@ -31,16 +32,22 @@ def captcha(img):
         return False
 
 
-def recaptcha(recaptcha_public_key):
+def recaptcha(site_key, url):
     """Generates recaptcha & resolves.
     Returns (recaptcha_challenge_field, recaptcha_response_field).
     """
+    recaptcha_answer = 'CAPCHA_NOT_READY'
     r = browser()
-    rc = r.get('http://www.google.com/recaptcha/api/challenge?k=%s&ajax=1&cachestop=%s' % (recaptcha_public_key, random())).text
-    recaptcha_challenge = re.search("challenge : '(.+)',", rc).group(1)
-    captcha_img = r.get('http://www.google.com/recaptcha/api/image?c=%s' % (recaptcha_challenge)).content
+    rc = r.post('http://2captcha.com/in.php?key={}&method=userrecaptcha&googlekey={}&pageurl={}'.format(twocaptcha_api_key, site_key, url)).text
+    print(rc)
+    captcha_id = rc.split('|')[1]
+    while 'CAPCHA_NOT_READY' in recaptcha_answer:
+        time.sleep(5)
+        recaptcha_answer = r.get("http://2captcha.com/res.php?key={}&action=get&id={}".format(twocaptcha_api_key, captcha_id)).text
+        # print(recaptcha_answer)
+    recaptcha_answer = recaptcha_answer.split('|')[1]
 
-    return recaptcha_challenge, captcha(captcha_img)
+    return recaptcha_answer
 
 
 def recaptchaReportWrong():
--- gdown/modules/realdebrid.py
+++ gdown/modules/realdebrid.py
@@ -8,17 +8,18 @@ This module contains handlers for realdebrid.
 
 """
 
-import re
+# import re
 import time
-from dateutil import parser
-from datetime import datetime
+# from dateutil import parser
+# from datetime import datetime
 # from bs4 import BeautifulSoup
 
+from ..core import recaptcha
 from ..module import browser, acc_info_template
 from ..exceptions import ModuleError
 
 
-def accInfo(username, passwd, proxy=False):
+def accInfo(username, passwd, proxy=False, captcha_response=False):
     """Returns account info."""
     acc_info = acc_info_template()
     r = browser(proxy)
@@ -28,17 +29,29 @@ def accInfo(username, passwd, proxy=False):
               'pin_challenge': '',
               'pin_answer': 'PIN: 000000',
               'time': int(time.time() * 1000)}
+    if captcha_response:
+        params['captcha_response'] = captcha_response
     rc = r.get('https://real-debrid.com/ajax/login.php', params=params)
-    open('gdown.log', 'w').write(rc.text)
+    open('gdown2.log', 'w').write(rc.text)
     rc = rc.json()
     if rc['message'] == 'Too many attempts':
         # {"error":3,"message":"Too many attempts","captcha":1,"captcha_type":"recaptcha","recaptcha_public_key":"6LeK8voSAAAAAEnlRaMgmux_r2hEMtNAdlUbuPbs","pin":0}
-        raise ModuleError('captcha')
+        # raise ModuleError('captcha')
+        ans = recaptcha(rc['recaptcha_public_key'], 'https://real-debrid.com/ajax/login.php')
+        return accInfo(username=username, passwd=passwd, proxy=proxy, captcha_response=ans)
     elif rc['message'] == 'Your login informations are incorrect !':
         acc_info['status'] = 'deleted'
     elif rc['message'] == 'PIN Code required':
-        print('pin')
+        print('PIN')
         acc_info['status'] = 'deleted'
+    elif rc['message'] == 'OK':
+        del r.headers['x-requested-with']
+        rc = r.get('https://real-debrid.com').text
+        open('gdown2.log', 'w').write(rc)
+        if 'Premium :<span class="fidelity">Free</span>' in rc:
+            acc_info['status'] = 'free'
+        else:
+            raise ModuleError('unknown status')
     else:
         raise ModuleError('unknown error')
     return acc_info
