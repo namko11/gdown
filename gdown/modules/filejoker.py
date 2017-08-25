c0637f13f1b18a17924b91cfd2660efdb15b0973
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-08-25 06:34:10 +0000

    filejoker: fix transfer detection & wait 5s on ip ban

--- gdown/modules/filejoker.py
+++ gdown/modules/filejoker.py
@@ -9,9 +9,10 @@ This module contains handlers for filejoker.
 """
 
 import re
-# import time
+import time
 from dateutil import parser
 # from datetime import datetime
+from requests.exceptions import ConnectionError
 
 from ..module import browser, acc_info_template
 from ..exceptions import ModuleError
@@ -26,19 +27,24 @@ def accInfo(username, passwd, proxy=False):
             'op': 'login',
             'redirect': '',
             'rand': ''}
-    rc = r.post('https://filejoker.net/login', data=data).text
+    try:
+        rc = r.post('https://filejoker.net/login', data=data).text
+    except ConnectionError as e:
+        print(e)
+        time.sleep(5)
+        return accInfo(username=username, passwd=passwd, proxy=proxy)
     open('gdown.log', 'w').write(rc)
 
     if 'Why do I have to complete a CAPTCHA?' in rc:
         # print('captcha')
         # time.sleep(5)
         # return accInfo(username=username, passwd=passwd, proxy=proxy)
-        raise ModuleError('captcha')
+        raise ModuleError('recaptcha')
     elif 'Incorrect Login or Password' in rc or 'Invalid email' in rc:
         acc_info['status'] = 'deleted'
     elif 'Premium account expires:' in rc:
         acc_info['status'] = 'premium'
-        acc_info['transfer'] = re.search('<td>Traffic Available:</td>\n<td>([0-9 MGB]+?)</td>', rc).group(1)
+        acc_info['transfer'] = re.search('<td>Traffic Available:</td>\n<td>([\-0-9 MGB]+?)</td>', rc).group(1)
         expire_date = re.search('Premium account expires: ([0-9 a-zA-Z]+?)\n', rc).group(1)
         acc_info['expire_date'] = parser.parse(expire_date)
     elif 'Buy Premium' in rc:  # kinda blind guess
